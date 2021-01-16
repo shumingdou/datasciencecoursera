cb660242a5f2fdc4019df4e0929191b4e1a128d3
shumingdou <76080605+shumingdou@users.noreply.github.com>
2021-01-16 07:52:42 -0600

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,61 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+## This function creates a special "matrix" object that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+
+##creates a special 'matrix' object that can cache its inverse
+  
+  m <- NULL #set an empty inverse matrix
+  set <- function(y) {
+    
+##sets the value of the matrix (x==y) to the parent env
+    
+    x <<- y
+    m <<- NULL #set an empty inverse matrix in the parent env
+  }
+  
+  get <- function() x #gets the value of the matrix
+  setInv <- function(inverse) m <<- inverse
+  
+##sets the value of the inverse matrix (m) in the parent env
+  
+  getInv <- function() m #gets the value of the inverse matrix
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
+  
+}
+
+
+## Write a short comment describing this function
+
+## cacheSolve: This function computes the inverse of the special
+## "matrix" returned by makeCacheMatrix above. If the inverse has 
+## already been calculated (and the matrix has not changed), then
+## cacheSolve should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+      ## Return a matrix that is the inverse of 'x'
+  
+##computes the inverse of the special "matrix" returned by makeCacheMatrix above
+##if the inverse has already been calculated (and the matrix has not changed),
+##then the cachesolve should retrieve the inverse from the cache
+  
+  m <- x$getInv() #gets the value of the inverse matrix
+  if(!is.null(m)) {
+    
+##if m already exists (the inverse is already calculated)
+    
+    message('getting cached data')
+    return(m)
+  }
+  
+##if m doesn't exist, calculate the inverse of the matrix
+  
+  data <- x$get() #gets the value of the matrix
+  m <- solve(data, ...) #calculates the inverse
+  x$setInv(m)
+  
+}
